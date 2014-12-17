04cbaa340c0d1618a8b4eacb5e9ce6dcff1c686b
aguiraf <aguiraf@gmail.com>
2014-12-17 15:58:21 +0100

    Configure to work in 5T

--- config.js
+++ config.js
@@ -1,12 +1,13 @@
 window.OTP_config = {
 
-  initLatLng: [42.703632059618045, -73.7948226928711],
+  initLatLng: [45.07,7.67],
 
   osmMapKey: 'conveyal.ikck6888', // temporary -- do not use in production, provide your own
   aerialMapKey: 'conveyal.map-a3mk3jug', // unset
 
-  otpApi: 'http://otp-test.nysdot.conveyal.com/otp/routers/',
-  esriApi: 'https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/',
+  //otpApi: 'http://supremo:8080/otp/ws/routers/',
+  otpApi: 'http://oltrepo:8080/otp/routers/',
+  //esriApi: 'https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/',
 
-  reverseGeocode: true
+  reverseGeocode: false
 };
