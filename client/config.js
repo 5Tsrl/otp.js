e1e8cc8ca7a81cc212c64f86e9b46ac032e68ba6
aguiraf <aguiraf@gmail.com>
2015-11-24 14:24:57 +0100

    Config initLatLng and otpApi

--- client/config.js
+++ client/config.js
@@ -1,10 +1,10 @@
 window.OTP_config = {
-  initLatLng: [38.880148, -77.105933],
+  initLatLng: [45.07,7.67],
 
   osmMapKey: 'conveyal.ikck6888', // temporary -- do not use in production, provide your own
   aerialMapKey: 'conveyal.map-a3mk3jug', // unset
 
-  otpApi: 'http://192.168.59.103:8080/otp/routers/',
+  otpApi: 'http://oltrepo:8080/otp/routers/',
   esriApi: 'https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/',
 
   reverseGeocode: true,
